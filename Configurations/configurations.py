a996aed43e570d77290550aebb456ecd9f01ec1b
yasanthaniroshan <niroshanyi@gmail.com>
2023-10-05 21:32:50 +0530

    auth system has completed

--- Configurations/configurations.py
+++ Configurations/configurations.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
 import os
+from flask import request, redirect, url_for, session
+from functools import wraps
+from flask import Blueprint,render_template,session,redirect
+
+
 
 global_configurations = {
     'PERMANENT_SESSION_LIFETIME':60
@@ -25,3 +30,15 @@ def get_secret_key()->str:
 def get_database_configurations()->dict:
     database_configurations['password'] = os.environ.get("MYSQL_PASSWORD")
     return database_configurations
+
+
+
+def valid_session(view_func):
+    @wraps(view_func)
+    def wrapped_view(*args, **kwargs):
+        if 'user' not in session :
+            print('user not in session')
+            return redirect('/auth/login')
+        print('user in session')
+        return view_func(*args, **kwargs)
+    return wrapped_view
