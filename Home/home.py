3faa1da2165f6c36428e24fd21bf03f266263076
dilumin <77558016+dilumin@users.noreply.github.com>
2023-10-09 11:15:03 +0530

    Added frontend files only

--- Home/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint,render_template
-from Settings.settings import *
-
-
-home_app = Blueprint('auth', __name__,template_folder='templates')
-
-@home_app.route('/',methods = DEFAULT_METHODS,endpoint='home')
-def home():
-    return render_template('home.html')
