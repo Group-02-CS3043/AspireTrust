741863bc96a63da7c7c9e50811817457391adf10
yasanthaniroshan <niroshanyi@gmail.com>
2023-10-12 19:59:22 +0530

    Intialize the Database quaries repo

--- Configurations/Error_handler.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template,Flask
-
-
-def handle_error(app:Flask):
-    @app.errorhandler(404)
-    def not_found(error):
-        return render_template('notfound.html'), 404
-    
-    return app
